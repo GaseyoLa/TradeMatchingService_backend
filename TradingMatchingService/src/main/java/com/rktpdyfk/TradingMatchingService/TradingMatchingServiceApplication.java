451c44e90b73c338697fa09b7d387ec80dee62e5
OneCrazyman <cjw7155@naver.com>
2024-01-11 18:30:51 +0900

    first commit

---
+++ TradingMatchingService/src/main/java/com/rktpdyfk/TradingMatchingService/TradingMatchingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rktpdyfk.TradingMatchingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingMatchingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingMatchingServiceApplication.class, args);
+	}
+
+}
---
+++ TradingMatchingService/src/test/java/com/rktpdyfk/TradingMatchingService/TradingMatchingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rktpdyfk.TradingMatchingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingMatchingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
