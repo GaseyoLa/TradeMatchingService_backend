f837a613750a05713361ccd785b0736cade4502c
OneCrazyman <cjw7155@naver.com>
2024-01-15 22:46:21 +0900

    feat: Category,item,itemC,OptionItem 엔티티 구현

--- TradingMatchingService/src/main/java/com/rktpdyfk/TradingMatchingService/Entity/Item.java
+++ TradingMatchingService/src/main/java/com/rktpdyfk/TradingMatchingService/Entity/Item.java
@@ -1,4 +1,24 @@
 package com.rktpdyfk.TradingMatchingService.Entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "item")
 public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @OneToOne
+    @JoinColumn(name = "option_item_id", unique = true, nullable = false)
+    private OptionItem optionItem;
 }
--- TradingMatchingService/src/main/java/com/rktpdyfk/TradingMatchingService/Entity/ItemCategory.java
+++ TradingMatchingService/src/main/java/com/rktpdyfk/TradingMatchingService/Entity/ItemCategory.java
@@ -1,4 +1,25 @@
 package com.rktpdyfk.TradingMatchingService.Entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
+@Table(name = "item_category")
 public class ItemCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_category_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", insertable = false, updatable = false, nullable = false)
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "Item_id", insertable = false, updatable = false, nullable = false)
+    private Item item;
+
 }
