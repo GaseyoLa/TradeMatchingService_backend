59ef6cb056867faf73de09242c6344cbc57f95b0
JRingterm <ghwns3712@naver.com>
2024-01-16 21:27:25 +0900

    [feat] repository 추가

---
+++ TradingMatchingService/src/main/java/com/rktpdyfk/TradingMatchingService/Repository/ItemCategoryRepository.java
@@ -0,0 +1,7 @@
+package com.rktpdyfk.TradingMatchingService.Repository;
+
+import com.rktpdyfk.TradingMatchingService.Entity.ItemCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemCategoryRepository extends JpaRepository<ItemCategory, Long> {
+}
---
+++ TradingMatchingService/src/main/java/com/rktpdyfk/TradingMatchingService/Repository/PostRepository.java
@@ -0,0 +1,7 @@
+package com.rktpdyfk.TradingMatchingService.Repository;
+
+import com.rktpdyfk.TradingMatchingService.Entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
