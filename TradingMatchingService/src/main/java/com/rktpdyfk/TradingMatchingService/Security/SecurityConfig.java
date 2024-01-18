ecac244ad617a5ea4690adf35380feed3080b7f5
OneCrazyman <cjw7155@naver.com>
2024-01-18 18:39:46 +0900

    feat: security/jwt jwt 파일 생성

--- TradingMatchingService/src/main/java/com/rktpdyfk/TradingMatchingService/Security/SecurityConfig.java
+++ TradingMatchingService/src/main/java/com/rktpdyfk/TradingMatchingService/Security/SecurityConfig.java
@@ -16,26 +16,22 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.filter.CorsFilter;
 
 //이곳에 jwt 클래스를 적용시킴
 @Configuration
 @EnableWebSecurity //
 @EnableMethodSecurity //
 public class SecurityConfig {
-    private final CorsFilter corsFilter;
     private final TokenProvider tokenProvider;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
     //jwt 3개 클래스를 주입받음
     public SecurityConfig(
-            CorsFilter corsFilter, TokenProvider tokenProvider,
+            TokenProvider tokenProvider,
             JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
             JwtAccessDeniedHandler jwtAccessDeniedHandler
     ) {
-        this.corsFilter = corsFilter;
         this.tokenProvider = tokenProvider;
         this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
         this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
@@ -55,7 +51,6 @@ public class SecurityConfig {
                 // token을 사용하는 방식이기 때문에 csrf를 disable합니다.
                 //corsfilter
                 .csrf(AbstractHttpConfigurer::disable)
-                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling(exceptionHandling->exceptionHandling
                         .accessDeniedHandler(jwtAccessDeniedHandler)
                         .authenticationEntryPoint(jwtAuthenticationEntryPoint)
