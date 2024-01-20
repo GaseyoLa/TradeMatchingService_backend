2cf5446657e8e266324eee59db38db4e906d9aee
OneCrazyman <cjw7155@naver.com>
2024-01-20 23:46:54 +0900

    refacter/jwt: User엔티티 @Builder.Default 추가

--- TradingMatchingService/src/main/resources/data.sql
+++ TradingMatchingService/src/main/resources/data.sql
@@ -1,5 +1,5 @@
-insert into "USERS" (discord_id, worlds_id, star, date, email, username, password, nickname, activated) values ("dic_admin","wor_admin","3.0",CURRENT_TIMESTAMP,'adm@nn.c','admin','$2a$08$lDnHPz7eUkSi6ao14Twuau08mzhWrL4kyZGGU5xfiGALO/Vxd5DOi', 'admin', 1);
-insert into "USERS" (discord_id, worlds_id, star, date, email, username, password, nickname, activated) values ("dic_user","wor_user","5.0",CURRENT_TIMESTAMP,'use@nn.c','user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC', 'user', 1);
+insert into "USERS" (discord_id, worlds_id, star, date, email, username, password, nickname, activated) values ("dic_admin","wor_admin",3,CURRENT_TIMESTAMP,'adm@nn.c','admin','$2a$08$lDnHPz7eUkSi6ao14Twuau08mzhWrL4kyZGGU5xfiGALO/Vxd5DOi', 'admin', 1);
+insert into "USERS" (discord_id, worlds_id, star, date, email, username, password, nickname, activated) values ("dic_user","wor_user",5,CURRENT_TIMESTAMP,'use@nn.c','user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC', 'user', 1);
 
 insert into authority (authority_name) values ('ROLE_USER');
 insert into authority (authority_name) values ('ROLE_ADMIN');
