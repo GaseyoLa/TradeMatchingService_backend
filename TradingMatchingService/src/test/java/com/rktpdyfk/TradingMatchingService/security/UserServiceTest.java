6111a451f8cec46f8f15e8e88b102d0be963d84f
OneCrazyman <cjw7155@naver.com>
2024-01-23 01:06:13 +0900

    Test/register: signup 중복테스트

--- TradingMatchingService/src/test/java/com/rktpdyfk/TradingMatchingService/security/UserServiceTest.java
+++ TradingMatchingService/src/test/java/com/rktpdyfk/TradingMatchingService/security/UserServiceTest.java
@@ -1,11 +1,27 @@
 package com.rktpdyfk.TradingMatchingService.security;
 
+import com.rktpdyfk.TradingMatchingService.dto.UserDto;
+import com.rktpdyfk.TradingMatchingService.entity.Authority;
+import com.rktpdyfk.TradingMatchingService.repository.UserRepository;
+import com.rktpdyfk.TradingMatchingService.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
+@SpringBootTest
 public class UserServiceTest {
+    @Autowired UserRepository userRepository;
+    @Autowired PasswordEncoder passwordEncoder;
+    @Autowired UserService userService;
+
+    //    @BeforeEach
+//    public void beforeEach(){
+//        userService = new UserService(userRepository, passwordEncoder);
+//    }
 
     @Test
     public void 암호_인코딩_테스트(){
@@ -20,4 +36,23 @@ public class UserServiceTest {
         System.out.println("Encoded Password: " + encodedPassword);
     }
 
-}
+    @Test
+    public void 회원가입_아이디_중복_테스트(){
+
+        //given
+        Authority authority = Authority.builder().authorityName("ROLE_USER").build();
+
+        UserDto userDto_one = new UserDto("user_one","user1234","user_nick","user@email.com",null,null,null);
+        UserDto userDto_two = new UserDto("user_one","user1234","user2_nick","user2@email.com",null,null,null);
+
+        // when
+        userService.signup(userDto_one);
+        //userService.signup(userDto_two);
+        RuntimeException e = assertThrows(RuntimeException.class, () ->
+            userService.signup(userDto_two));
+
+        // then
+        assertThat(e.getMessage()).isEqualTo("이미 가입되어 있는 유저입니다. 아이디 중복");
+        }
+
+    }
