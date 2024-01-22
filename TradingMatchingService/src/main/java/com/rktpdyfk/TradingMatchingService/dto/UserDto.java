0cd4975df0978a760ca23b0019f7a2a7524d736a
OneCrazyman <cjw7155@naver.com>
2024-01-22 21:57:56 +0900

    feat: 암호_인코딩_테스트 코드 추가

--- TradingMatchingService/src/main/java/com/rktpdyfk/TradingMatchingService/dto/UserDto.java
+++ TradingMatchingService/src/main/java/com/rktpdyfk/TradingMatchingService/dto/UserDto.java
@@ -18,26 +18,26 @@ import java.util.stream.Collectors;
 public class UserDto {
 
    @NotNull
-   @Size(min = 3, max = 50)
+   @Size(min = 2, max = 50)
    private String username;
 
    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
    @NotNull
-   @Size(min = 3, max = 100)
+   @Size(min = 8, max = 100)
    private String password;
 
    @NotNull
-   @Size(min = 3, max = 50)
+   @Size(min = 2, max = 50)
    private String nickname;
 
    @NotNull
    @Size(min = 3, max = 50)
    private String email;
 
-   @Size(min = 3, max = 50)
+   @Size(min = 2, max = 50)
    private String discordId;
 
-   @Size(min = 3, max = 50)
+   @Size(min = 2, max = 50)
    private String worldsId;
 
    private Set<AuthorityDto> authorityDtoSet;
---
+++ TradingMatchingService/src/test/java/com/rktpdyfk/TradingMatchingService/security/UserServiceTest.java
@@ -0,0 +1,23 @@
+package com.rktpdyfk.TradingMatchingService.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class UserServiceTest {
+
+    @Test
+    public void 암호_인코딩_테스트(){
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+        String rawPassword = "admin1234";
+        String encodedPassword = passwordEncoder.encode(rawPassword);
+
+        assertNotNull(encodedPassword);
+
+        System.out.println("Raw Password: " + rawPassword);
+        System.out.println("Encoded Password: " + encodedPassword);
+    }
+
+}
