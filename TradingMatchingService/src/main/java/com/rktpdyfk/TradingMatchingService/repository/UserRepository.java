5e3c95a45e9a002d5c1cb6d799bacbdd5034d283
OneCrazyman <cjw7155@naver.com>
2024-01-23 01:06:13 +0900

    Test/register: signup 중복테스트

--- TradingMatchingService/src/main/java/com/rktpdyfk/TradingMatchingService/repository/UserRepository.java
+++ TradingMatchingService/src/main/java/com/rktpdyfk/TradingMatchingService/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.rktpdyfk.TradingMatchingService.repository;
 import com.rktpdyfk.TradingMatchingService.entity.User;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     //Lazy조회가 아닌 Eager조회로 auth 정보를 같이 가져옴.
     @EntityGraph(attributePaths = "authorities")
