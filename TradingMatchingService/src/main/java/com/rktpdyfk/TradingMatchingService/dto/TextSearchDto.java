c3b9795bae2fcc95175155c68f2103fb94b7e17f
OneCrazyman <cjw7155@naver.com>
2024-03-11 22:36:54 +0900

    refacter: Dto 생성자 추가

--- TradingMatchingService/src/main/java/com/rktpdyfk/TradingMatchingService/dto/TextSearchDto.java
+++ TradingMatchingService/src/main/java/com/rktpdyfk/TradingMatchingService/dto/TextSearchDto.java
@@ -1,5 +1,6 @@
 package com.rktpdyfk.TradingMatchingService.dto;
 
+import com.rktpdyfk.TradingMatchingService.entity.Item;
 import lombok.*;
 
 @Getter
@@ -11,7 +12,13 @@ public class TextSearchDto {
 
     private Long id;
 
-    private String name;
-
     private String image;
+
+    private String nameKr;
+
+    public TextSearchDto(Item item){
+        this.id = item.getId();
+        this.image = item.getImage();
+        this.nameKr = item.getNameKr();
+    }
 }
