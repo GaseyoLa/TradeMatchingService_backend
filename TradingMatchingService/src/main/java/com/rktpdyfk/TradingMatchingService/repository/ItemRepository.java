4cf8977a24652bbea40881c65596c0a0c2f3dcf7
OneCrazyman <cjw7155@naver.com>
2024-03-12 19:50:55 +0900

    feat: 특정 아이템 검색

--- TradingMatchingService/src/main/java/com/rktpdyfk/TradingMatchingService/repository/ItemRepository.java
+++ TradingMatchingService/src/main/java/com/rktpdyfk/TradingMatchingService/repository/ItemRepository.java
@@ -14,12 +14,10 @@ import java.util.Optional;
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
-    //아이템 이름으로 카테고리 객체 가져오기.
-//    @Query("SELECT i.category FROM Item i WHERE i.nameKr = :itemName")
-//    Optional<Category> findCategoryByItemName(@Param("itemName") String itemName);
-
     //TextSearch용
     @Query("SELECT i FROM Item i WHERE i.nameKr LIKE CONCAT('%',:keyword,'%')")
     List<Item> findItemListByKeyword(@Param("keyword") String keyword);
 
+    //특정 아이템 검색
+    Optional<Item> findOneByNameKr(String nameKr);
 }
