a1ce71943e10a671cfe0800691893610ed350318
OneCrazyman <cjw7155@naver.com>
2024-04-06 17:08:45 +0900

    test: 테스트 케이스 수정

--- TradingMatchingService/src/test/java/com/rktpdyfk/TradingMatchingService/service/PostServiceTest.java
+++ TradingMatchingService/src/test/java/com/rktpdyfk/TradingMatchingService/service/PostServiceTest.java
@@ -1,21 +1,20 @@
 package com.rktpdyfk.TradingMatchingService.service;
 
-import com.rktpdyfk.TradingMatchingService.dto.ItemDto;
 import com.rktpdyfk.TradingMatchingService.dto.PostDto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
 class PostServiceTest {
 
-    @Autowired PostService postService;
+    @Autowired
+    PostService postService;
+
     @Test
-    void getPost_테스트() {
-        PostDto.PostListResponseDto postResponseDto = postService.getPost(3L);
-        Assertions.assertThat(postResponseDto.getPostId()).isEqualTo(3L);
+    void getPost() {
+        PostDto.PostListResponseDto postListResponseDto = postService.getPost(1L);
+        Assertions.assertThat(postListResponseDto.getItemId()).isEqualTo(1002005);
     }
 }
